37aa505f6dc17456b27753e8b6299bcd0ec1e47a
Roland Enola <aloneroland@gmail.com>
2022-02-20 17:52:33 -0800

    new changes

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -13,8 +13,9 @@ contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    string baseSvg =
+    string svgPartOne =
         "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+    string svgPartTwo ="'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
     string[] firstWords = [
         "Fantastic",
@@ -43,6 +44,10 @@ contract MyEpicNFT is ERC721URIStorage {
         "Yuna",
         "Madara"
     ];
+
+    string[] colors = ["red", "black", "yellow", "green", "blue", "#08C2A8"];
+
+    event NewEpicNFTMinted(address sender, uint256 tokenId);
 
     constructor() ERC721("SquareNFT", "SQUARE") {
         console.log("I am a smart contract!");
@@ -84,6 +89,18 @@ contract MyEpicNFT is ERC721URIStorage {
         return thirdWords[rand];
     }
 
+     function pickRandomColor(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("COLOR", Strings.toString(tokenId)))
+        );
+        rand = rand % colors.length;
+        return thirdWords[rand];
+    }
+
     function random(string memory input) internal pure returns (uint256) {
         return uint256(keccak256(abi.encodePacked(input)));
     }
@@ -98,8 +115,10 @@ contract MyEpicNFT is ERC721URIStorage {
             abi.encodePacked(first, second, third)
         );
 
+        string memory randomColor = pickRandomColor(newItemId);
+
         string memory finalSvg = string(
-            abi.encodePacked(baseSvg, first, second, third, "</text></svg>")
+            abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>")
         );
 
         string memory json = Base64.encode(
@@ -137,5 +156,7 @@ contract MyEpicNFT is ERC721URIStorage {
             newItemId,
             msg.sender
         );
+
+        emit NewEpicNFTMinted(msg.sender, newItemId);
     }
 }
